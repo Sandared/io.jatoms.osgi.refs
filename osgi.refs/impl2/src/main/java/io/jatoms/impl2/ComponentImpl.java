8ffee786c4d4aa448874216486a4a48dcd81dc3b
Sandared <thomas.driessen.td@gmail.com>
2019-02-15 17:47:24 +0000

    Added additional impl module

---
+++ osgi.refs/impl2/src/main/java/io/jatoms/impl2/ComponentImpl.java
@@ -0,0 +1,10 @@
+package io.jatoms.impl2;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class ComponentImpl {
+    
+    //TODO add an implementation
+    
+}
---
+++ osgi.refs/impl2/src/test/java/io/jatoms/impl2/UnitTest.java
@@ -0,0 +1,12 @@
+package io.jatoms.impl2;
+
+import org.junit.Test;
+
+public class UnitTest {
+    
+    @Test
+    public void testSomething() {
+        //TODO add an implementation
+    }
+    
+}
