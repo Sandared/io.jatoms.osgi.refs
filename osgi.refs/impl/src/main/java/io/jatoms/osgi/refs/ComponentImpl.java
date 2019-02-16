c719efc3a3ea55d7358048e8e0c506ff268e0009
Sandared <thomas.driessen.td@gmail.com>
2019-02-16 14:06:36 +0000

    minor changes

--- osgi.refs/impl/src/main/java/io/jatoms/osgi/refs/ComponentImpl.java
+++ osgi.refs/impl/src/main/java/io/jatoms/osgi/refs/ComponentImpl.java
@@ -6,25 +6,26 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 @Component
 public class ComponentImpl {
+    // @Reference(cardinality=ReferenceCardinality.OPTIONAL, policy=ReferencePolicy.STATIC, policyOption=ReferencePolicyOption.GREEDY)
+    private volatile ITest atest;
 
-    @Reference(policyOption=ReferencePolicyOption.GREEDY)
+    @Reference(cardinality=ReferenceCardinality.MULTIPLE, policy=ReferencePolicy.DYNAMIC, policyOption=ReferencePolicyOption.RELUCTANT)
     private List<ITest> tests;
 
-    // @Reference(policyOption=ReferencePolicyOption.GREEDY)
-    private ITest test;
 
     @Activate
     void activate () {
         System.out.println("#### ACTIVATE START ####");
-        System.out.println("TEST: " + test);
-        System.out.println("TESTS == NULL: " + (tests == null));
-        System.out.println("TESTS: " + tests);
+        System.out.println("SINGLE REF: " + atest);
+        System.out.println("LIST REF: " + tests);
         if(tests != null){
-            System.out.println("TESTS TYPE: " + tests.getClass().getName());
+            System.out.println("LIST TYPE: " + tests.getClass().getName());
         }
         System.out.println("#### ACTIVATE END ####");
     }
@@ -32,11 +33,10 @@ public class ComponentImpl {
     @Deactivate
     void deactivate () {
         System.out.println("#### DEACTIVATE START ####");
-        System.out.println("TEST: " + test);
-        System.out.println("TESTS == NULL: " + (tests == null));
-        System.out.println("TESTS: " + tests);
+         System.out.println("SINGLE REF: " + atest);
+        System.out.println("LIST REF: " + tests);
         if(tests != null){
-            System.out.println("TESTS TYPE: " + tests.getClass().getName());
+            System.out.println("LIST TYPE: " + tests.getClass().getName());
         }
         System.out.println("#### DEACTIVATE END ####");
     }
